124c09234d94780a8b1ef8d1e48f3c78ab59ef6d
marisiromanillos <marisiromanillos@gmail.com>
2025-02-13 18:18:27 +0000

    get, post, delete operations for expenses api route

---
+++ changelog.md
@@ -0,0 +1,14 @@
+# Changelog
+
+
+## [Unreleased]
+
+### Added
+- Zod
+- Hono Zod validator
+- Created get, post, delete operations for expenses api route
+
+### Changed
+
+
+### Removed
--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,16 +1,20 @@
 import { Hono } from "hono";
 import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 
 // Expenses type
-type Expense = {
-    id: number,
-    title: string,
-    amount: number,
-    expenseType: string
-}
+const expenseSchema = z.object({
+    id: z.number().int().positive().min(1),
+    title: z.string().min(3).max(100),
+    amount: z.number().int().positive(),
+    expenseType: z.string().min(3).max(100)
+})
+
+type Expense = z.infer<typeof expenseSchema>
+
+const createPostSchema = expenseSchema.omit({id: true})
 
 // Fake expenses
-
 const fakeExpenses: Expense[] = [
     {
       id: 1,
@@ -51,19 +55,31 @@ const fakeExpenses: Expense[] = [
     }
 ];
 
-const createPostSchema = z.object({
-    title: z.string(),
-    amount: z.number(),
-    expenseType: z.string()
-})
 
 export const expensesRoute = new Hono()
     .get("/", (c) => {
         return c.json({ expenses: fakeExpenses});
     })
-    .post("/", async (c) => {
-        const data = await c.req.json()
-        const expense = createPostSchema.parse(data)
-        console.log({expense})
+    .post("/", zValidator("json", createPostSchema), async (c) => {
+        const expense = await c.req.valid("json")
+        fakeExpenses.push({...expense, id: fakeExpenses.length+1})
+        c.status(201)
         return c.json(expense);
+    })
+    .get("/:id{[0-9]+}", (c) => {
+        const id = Number.parseInt(c.req.param('id'));
+        const expense = fakeExpenses.find(expense => expense.id === id)
+        if (!expense){
+            return c.notFound()
+        }
+        return c.json({expense})
+    })
+    .delete("/:id{[0-9]+}", (c) => {
+        const id = Number.parseInt(c.req.param("id"));
+        const index = fakeExpenses.findIndex(expense => expense.id === id)
+        if(index === -1){
+            return c.notFound()
+        }
+        const deletedExpense = fakeExpenses.splice(index, 1)[0];
+        return c.json({ expense: deletedExpense});
     })
